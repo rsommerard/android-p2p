33cea27be187e141345ecfc90b10c548a781896a
Romain SOMMERARD <rsommerard@gmail.com>
2015-11-20 16:07:38 +0100

    Add service buttons

--- P2PApplication/app/src/main/java/fr/rsommerard/p2papplication/MainActivity.java
+++ P2PApplication/app/src/main/java/fr/rsommerard/p2papplication/MainActivity.java
@@ -2,12 +2,41 @@ package fr.rsommerard.p2papplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "P2PApplication";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startServiceButton = (Button) findViewById(R.id.button_start_service);
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService();
+            }
+        });
+
+        Button stopServiceButton = (Button) findViewById(R.id.button_stop_service);
+        stopServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService();
+            }
+        });
+    }
+
+    public void startService() {
+        Log.d(TAG, "startService");
+    }
+
+    public void stopService() {
+        Log.d(TAG, "stopService");
     }
 }
